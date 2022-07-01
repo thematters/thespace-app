91cb7edc5c6ca5ebef4bec1cb4999039451cc79d
zxygentoo <zxy@matters.news>
2022-07-01 10:57:34 +0000

    prod release: switch to maticvigil

--- src/Config/Network/Mainnet.elm
+++ src/Config/Network/Mainnet.elm
@@ -5,7 +5,8 @@ import Config.Env.Util exposing (RpcProvider)
 
 rpcHttpAddress : String
 rpcHttpAddress =
-    "https://polygon-rpc.com"
+    --"https://polygon-rpc.com"
+    "https://rpc-mainnet.maticvigil.com/"
 
 
 blockExplorerUrls : String
